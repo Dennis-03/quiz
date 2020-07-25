a9ba220a394183061a71418cdac37dd684498bc0
Dennisovich-03 <54091928+Dennisovich-03@users.noreply.github.com>
2020-07-25 20:46:56 +0530

    object destructured

---
+++ src/components/answer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function answer(props) {
+  return <option>{props.answer}</option>;
+}
+
+export default answer;
---
+++ src/components/questions.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+
+class questions extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: [
+        {
+          question:
+            "While working with a web app, you might need to use various form controls such as text boxes, checkboxes, dropdowns, file uploads, or radio buttons in order to use HTML elements or third-party libraries for React, such as material-ui.",
+          option1: "answer1",
+          option2: "answer2",
+          option3: "answer3",
+          option4: "answer4",
+        },
+        {
+          question:
+            "working with a web app, you might need to use various form controls such as text boxes, checkboxes, dropdowns, file uploads, or radio buttons in order to use HTML elements or third-party libraries for React, such as material-ui.",
+          option1: "answer1",
+          option2: "answer2",
+          option3: "answer3",
+          option4: "answer4",
+        },
+        {
+          question:
+            "ith a web app, you might need to use various form controls such as text boxes, checkboxes, dropdowns, file uploads, or radio buttons in order to use HTML elements or third-party libraries for React, such as material-ui.",
+          option1: "answer1",
+          option2: "answer2",
+          option3: "answer3",
+          option4: "answer4",
+        },
+        {
+          question:
+            "need to use various form controls such as text boxes, checkboxes, dropdowns, file uploads, or radio buttons in order to use HTML elements or third-party libraries for React, such as material-ui.",
+          option1: "answer1",
+          option2: "answer2",
+          option3: "answer3",
+          option4: "answer4",
+        },
+      ],
+      qno: 0,
+      noofquestions: 3,
+      answers: [],
+    };
+    this.onValueChange = this.onValueChange.bind(this);
+    this.formSubmit = this.formSubmit.bind(this);
+  }
+  onValueChange(event) {
+    this.setState({
+      selectedOption: event.target.value,
+    });
+  }
+
+  formSubmit(event) {
+    let value = this.state.answers;
+    value.push(this.state.selectedOption);
+    event.preventDefault();
+    console.log(this.state.selectedOption);
+    this.setState((prevstate) => ({
+      qno: prevstate.qno + 1,
+      answers: value,
+      selectedOption: "",
+    }));
+    if (this.state.qno === this.state.noofquestions) {
+      console.log(this.state.answers);
+    }
+  }
+  render() {
+    const { results, qno, selectedOption } = this.state;
+    return (
+      <div className="container">
+        <form onSubmit={this.formSubmit}>
+          <div className="card question-container">
+            <p>
+              <strong>QUESTION:</strong>
+              {results[qno].question}
+            </p>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value={results[qno].option1}
+                  checked={selectedOption === results[qno].option1}
+                  onChange={this.onValueChange}
+                  required
+                />
+                {results[qno].option1}
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value={results[qno].option2}
+                  checked={selectedOption === results[qno].option2}
+                  onChange={this.onValueChange}
+                />
+                {results[qno].option2}
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value={results[qno].option3}
+                  checked={selectedOption === results[qno].option3}
+                  onChange={this.onValueChange}
+                />
+                {results[qno].option3}
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value={results[qno].option4}
+                  checked={selectedOption === results[qno].option4}
+                  onChange={this.onValueChange}
+                />
+                {results[qno].option4}
+              </label>
+            </div>
+
+            {/* <div>Selected option is : {selectedOption}</div> */}
+          </div>
+          <button className="btn btn-success" type="submit">
+            Next
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+export default questions;
